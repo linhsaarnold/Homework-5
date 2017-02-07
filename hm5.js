dc6f7302ad733d834571a524fe5b89cc8f2ada2e
linhsaarnold <linhsa.arnold@gmail.com>
2017-02-07 13:11:39 -0800

    Commit 1 for February 7

---
+++ hm5.js
@@ -0,0 +1,20 @@
+var count = 0;
+
+function myFunction()
+{
+
+count++;
+
+document.getElementById('Total').innerHTML = "Total: $ " + count;
+
+}
+
+var count = 0;
+
+function myFunction2()
+{
+
+document.getElementById('Total').innerHTML = "Total: $"
+
+}
+
